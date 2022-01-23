d1b9f17d44369a8cd0c358e656e55a5b586c0d29
Sydney Mercier <sydney.mercier91@gmail.com>
2022-01-23 14:25:20 -0600

    writing data to file

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter project title: "
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter project description: "
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Enter installation instructions: "
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Enter usage information: "
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Enter contribution guidelines: "
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "Enter test instructions: "
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter your email: "
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.log(err) : console.log("Data written to file."));
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Create a function to initialize app
+function init() {
+    let fileName = "README.md";
+
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response);
+            writeToFile(fileName, response);
+        })
+}
 
 // Function call to initialize app
 init();
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return '';
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  
+  ## Installation
+  Run the following command:
+  ~~~
+  ${data.installation}
+  ~~~
 
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contribution}
+  
+  ## Tests
+  ${data.test}
+  
+  ## Questions
+  Please contact me with any questions at ${data.email}.
+  
+  ${renderLicenseSection(data.license)}
 `;
 }
 
